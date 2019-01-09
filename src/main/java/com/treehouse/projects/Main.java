45a18c05280fab6505cd9970a1095628994d31b9
bgarf <bethaniegarfin@gmail.com>
2019-01-09 20:38:19 +0000

    Add handlebar template and add files to .gitignore

--- src/main/java/com/treehouse/projects/Main.java
+++ src/main/java/com/treehouse/projects/Main.java
@@ -1,9 +1,15 @@
 package com.treehouse.projects;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args){
-        get("/hello", (req, res) -> "We've got a thiinnnngggg going onnnnnnn");
+        get("/", (req, res) -> {
+            return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
+
